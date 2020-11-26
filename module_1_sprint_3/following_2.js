db80bc75cce42eebe548684186ea31ef0b60e9ba
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2020-11-26 15:30:30 +0530

    sprint-3 dsa

---
+++ module_1_sprint_3/following_2.js
@@ -0,0 +1,13 @@
+function runProgram(input) {
+	input = input.split('\n')[1].split(' ').map(Number);
+	var index = input.indexOf(2);
+
+	if (index == -1 || index == input.length - 1 || input.length == 1) {
+		console.log(-1);
+	} else {
+		console.log(input[index + 1]);
+	}
+}
+
+runProgram(`5
+3 4 2 0 1`);
